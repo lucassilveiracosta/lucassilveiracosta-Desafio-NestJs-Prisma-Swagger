7dfe0de815490333457f2c8ebfbbc3739c6ff826
Lucas Costa <lscosta2007@gmail.com>
2026-02-03 00:39:06 -0300

    First commit: Documentation. For this api, i'm using the swagger for the documentation and started to run the project in localhost, just have to document some error, like 404 or 400.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() 
 {
@@ -10,6 +11,19 @@ async function bootstrap()
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  const config = new DocumentBuilder()
+    .setTitle(`Desafio-NestJs-Prisma-Swagger`)
+    .setDescription(`API utilizando NestJs e Swagger`)
+    .setVersion(`1.0`)
+    .addTag(`NestJS`)
+    .build();
+
+  const document = SwaggerModule.createDocument( app, config );
+  SwaggerModule.setup(`api`, app, document) //localhost:3000/api
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(`Aplicação rodando em: http://localhost:${process.env.PORT ?? 3000}`);
+  console.log(`Swagger disponível em: http://localhost:${process.env.PORT ?? 3000}/api`);
 }
 bootstrap();
